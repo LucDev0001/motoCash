22d509479098dc8edc9d24bf88b9911ca301e24f
LucDev0001 <156922180+LucDev0001@users.noreply.github.com>
2025-11-20 00:29:56 -0300

    chore(firebase): cria índice para ordenação de produtos no marketplace
    - Adiciona o índice necessário no Firestore para permitir a ordenação de produtos por data de criação (`createdAt` em ordem decrescente).
    - Esta ação corrige o bug que impedia os produtos de serem exibidos na tela do marketplace.
    - Atualiza a versão do cache para v3.2.26.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "motocash-v3.2.25"; // Versão do cache atualizada
+const CACHE_NAME = "motocash-v3.2.26"; // Versão do cache atualizada
 const URLS_TO_CACHE = [
   "/",
   "/index.html",
