95136a53cdcccc069537879d6b338379137eae5b
LucDev0001 <156922180+LucDev0001@users.noreply.github.com>
2025-11-19 22:37:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "motocash-v3.2.7"; // Versão do cache atualizada
+const CACHE_NAME = "motocash-v3.2.8"; // Versão do cache atualizada
 const URLS_TO_CACHE = [
   "/",
   "/index.html",
